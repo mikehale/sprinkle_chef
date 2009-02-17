d2ed054924851a57288ef5c72a0c87216267c705
Michael Hale <mikehale@gmail.com>
2009-02-17 10:19:40 -0500

    add chef solo to client

--- packages/chef_client.rb
+++ packages/chef_client.rb
@@ -3,6 +3,14 @@ require 'rb-gems'
 package :chef_client do
   gem 'chef' do
     pre :install, 'gem sources|grep opscode || gem sources -a http://gems.opscode.com'
+    post :install, 'mkdir -p /etc/chef'
+    post :install, %(cat > /etc/chef/solo.rb <<EOF
+log_level          :info
+log_location       STDOUT
+file_cache_path    "/tmp/chef-solo"
+cookbook_path      "/tmp/chef-solo/cookbooks"
+Chef::Log::Formatter.show_time = false
+EOF)
   end
   requires :ohai
   requires :rubygems
