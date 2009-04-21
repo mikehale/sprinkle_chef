970ca0f3cf7333bc748fa7ba8af266636ee9f494
Michael Hale <mikehale@gmail.com>
2009-04-21 15:30:30 -0400

    include openssl readline and zlib support

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -1,3 +1,10 @@
 package :ruby do
   source "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.6.tar.bz2"
+  custom_install "./configure --prefix=#{@options[:prefix]} --with-openssl-dir=/usr --with-readline-dir=/usr --with-zlib-dir=/usr && make && make install"
+  requires :zlib_dev
+  requires :libssl_dev
+  requires :libreadline_dev
 end
+package(:zlib_dev) { apt "zlib1g-dev" }
+package(:libssl_dev) { apt "libssl-dev" }
+package(:libreadline_dev) { apt "libreadline5-dev" }
--- packages/rubygems.rb
+++ packages/rubygems.rb
@@ -4,14 +4,10 @@ package :rubygems do
   source "http://rubyforge.org/frs/download.php/55066/rubygems-1.3.2.tgz" do
     pre :install, 'echo "gem: --no-rdoc --no-ri" >> /etc/gemrc;'
     custom_install 'ruby setup.rb'
-    # post :install, 'ln -nfs /usr/bin/gem1.8 /usr/bin/gem'
     post :install, 'gem sources -a http://gems.opscode.com'
     post :install, 'gem update'
     post :install, "gem update --system"
   end
 
   requires :ruby
-  requires :zlibc
 end
-
-package(:zlibc) { apt "zlibc" }
