6a86a73f2bdbf2194382e94d6f979d767e86b294
Michael Hale <mikehale@gmail.com>
2009-04-21 14:24:33 -0400

    include zlib-dev

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -1,3 +1,7 @@
 package :ruby do
   source "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.6.tar.bz2"
+  
+  requires :zlib_dev
 end
+
+package(:zlib_dev) { apt "zlib1g-dev" }
--- packages/rubygems.rb
+++ packages/rubygems.rb
@@ -4,16 +4,16 @@ package :rubygems do
   source "http://rubyforge.org/frs/download.php/55066/rubygems-1.3.2.tgz" do
     pre :install, 'echo "gem: --no-rdoc --no-ri" >> /etc/gemrc;'
     custom_install 'ruby setup.rb'
-    post :install, 'ln -nfs /usr/bin/gem1.8 /usr/bin/gem'
+    # post :install, 'ln -nfs /usr/bin/gem1.8 /usr/bin/gem'
     post :install, 'gem sources -a http://gems.opscode.com'
     post :install, 'gem update'
     post :install, "gem update --system"
   end
 
-  verify 'binary' do
-    has_file '/usr/bin/gem'
-    has_symlink '/usr/bin/gem', '/usr/bin/gem1.8'
-  end
+  # verify 'binary' do
+  #   has_file '/usr/bin/gem'
+  #   has_symlink '/usr/bin/gem', '/usr/bin/gem1.8'
+  # end
 
   requires :ruby
 end
