a26e6882df5071432ecb1fa9e534831760dd32df
Michael Hale <mikehale@gmail.com>
2009-05-04 11:56:54 -0400

    add missing quote

--- packages/chef_server.rb
+++ packages/chef_server.rb
@@ -3,7 +3,7 @@ package :chef_server do
     commands = []
     commands << 'mkdir -p /etc/chef'
     commands << 'mkdir -p /etc/chef'
-    commands << %(echo 'file_cache_path "/tmp/chef-solo\\ncookbook_path "/tmp/chef-solo/cookbooks"' > ~/solo.rb)
+    commands << %(echo 'file_cache_path "/tmp/chef-solo"\\ncookbook_path "/tmp/chef-solo/cookbooks"' > ~/solo.rb)
     2.times {
       commands << 'chef-solo -c ~/solo.rb -r http://s3.amazonaws.com/chef-solo/bootstrap-0.5.6.tar.gz'
     }
