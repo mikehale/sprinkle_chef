c7f1818eba3ef7e0a1b92ad823823269e9eca493
Michael Hale <mikehale@gmail.com>
2009-06-11 15:41:00 -0400

    latest ruby

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -1,5 +1,5 @@
 package :ruby do
-  source "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.6.tar.bz2"
+  source "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.6-p369.tar.bz2"
   custom_install "./configure --prefix=/usr/local --with-openssl-dir=/usr --with-readline-dir=/usr --with-zlib-dir=/usr && make && make install"
   requires :zlib_dev
   requires :libssl_dev
